5ad531e2af4a4c8a6ea9b3b2028fd63e7408509c
DRMus <ditrom7306@mail.ru>
2021-07-09 21:02:22 +0400

    delete comments

--- src/app/home-page/shared/services/candidates.service.ts
+++ src/app/home-page/shared/services/candidates.service.ts
@@ -7,29 +7,6 @@ import {Observable} from "rxjs";
 
 export class CandidatesService {
 
-  // technologies: Tech[] = [
-  //   {id: 1, technology: 'PHP', check: false, color: '#de3939'},
-  //   {id: 2, technology: 'Python', check: false, color: '#2a73c6'},
-  //   {id: 3, technology: 'Java', check: false, color: '#52d545'},
-  // ]
-  // technologies: Tech[] = [
-  //   {id: 1, technology: 'PHP', check: false },
-  //   {id: 2, technology: 'Python', check: false },
-  //   {id: 3, technology: 'Java', check: false },
-  // ]
-
-  // candidates: candidates[] = [
-  //   {id: 1,
-  //     f_i_o: 'Иванов Иван Иванович',
-  //     birth_date: '01.01.2021',
-  //     technologies: [
-  //       {name: 'PHP', level: 1 },
-  //       {name: 'Python', level: 2 },
-  //       {name: 'Java', level: 3 },
-  //     ],
-  //     description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.' }
-  // ]
-
   private http!: HttpClient
 
   constructor(http: HttpClient) {
