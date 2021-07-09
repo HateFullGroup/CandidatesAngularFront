4153a13da42aae7314bdb9607b28af7d5ad491e2
Levodex Censies <levodex@outlook.com>
2021-07-09 22:00:01 +0300

    Add logout button

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnChanges, OnInit} from '@angular/core';
 import {Router} from "@angular/router";
 import {TitleService} from "./shared/services/title.service";
+import {AuthService} from "../auth/services/auth.service";
 
 @Component({
   selector: 'app-home-page',
@@ -8,18 +9,20 @@ import {TitleService} from "./shared/services/title.service";
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent implements OnInit{
-
+  auth!: AuthService
   private rooter!: Router
   titleService!: TitleService
 
   date: Date = new Date()
 
-  constructor(rooter: Router, title: TitleService) {
+  constructor(auth: AuthService, rooter: Router, title: TitleService) {
     this.rooter = rooter
     this.titleService = title
+    this.auth = auth
   }
 
   ngOnInit(): void {
 
   }
+
 }
